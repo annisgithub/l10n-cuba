e2c691b359ad9e22198337c05b0ec88889b683f1
Annis <annis@countigo.cu>
2024-12-09 09:11:26 -0500

    Initial l10n_cu_movement

---
+++ l10n_cu_hr_movement/__manifest__.py
@@ -0,0 +1,18 @@
+# List of contributors:
+# Segu
+
+{
+     'name': 'Cuba - RRHH',
+     'version': '17.0',
+     'category': 'Human Resources',
+     'summary': """
+        Empleados, Movimientos de Nómina.
+     """,
+     'author': 'Comunidad Cubana de Odoo',
+     'depends': ["hr","l10n_cu","l10n_cu_hr"],
+     'auto_install': True,     
+     'data': [
+
+     ],
+     'license': 'LGPL-3',
+}
